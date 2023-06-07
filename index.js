dc029ee60e39e793eed3af32fcdb395bb90991f3
MariaTorubarova <trofimovamariaa@gmail.com>
2023-06-07 16:28:17 +0400

    fixed styles

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ function popupShow() {
 
 function closePopup() {
   let popup = document.querySelector(".popup_menu");
-  let popupG = document.querySelector(".popup_menu-gratitude");
+  // let popupG = document.querySelector(".popup_menu-gratitude");
   let overlay = document.querySelector(".overlay");
   popup.style.display = "none";
-  popupG.style.display = "none";
+  // popupG.style.display = "none";
   overlay.style.display = "none";
 
 }
