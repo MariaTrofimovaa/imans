4597e2331eb67f83c0d5669aff01c39da2bfdcf9
MariaTorubarova <trofimovamariaa@gmail.com>
2023-06-07 14:45:45 +0400

    add mobile + desktop, popup styles; popup show-close functions

---
+++ index.js
@@ -0,0 +1,33 @@
+function popupShow() {
+  let popup = document.querySelector(".popup_menu");
+  let overlay = document.querySelector(".overlay");
+  popup.style.display = "block";
+  overlay.style.display = "block";
+}
+
+function closePopup() {
+  let popup = document.querySelector(".popup_menu");
+  let popupG = document.querySelector(".popup_menu-gratitude");
+  let overlay = document.querySelector(".overlay");
+  popup.style.display = "none";
+  popupG.style.display = "none";
+  overlay.style.display = "none";
+
+}
+
+function overlayClose() {
+  let popup = document.querySelector(".popup_menu");
+  let popupG = document.querySelector(".popup_menu-gratitude");
+  let overlay = document.querySelector(".overlay");
+  popup.style.display = "none";
+  popupG.style.display = "none";
+  overlay.style.display = "none";
+}
+
+function gratitudePopupShow() {
+  closePopup();
+  let popup = document.querySelector(".popup_menu-gratitude");
+  let overlay = document.querySelector(".overlay");
+  popup.style.display = "block";
+  overlay.style.display = "block";
+}
